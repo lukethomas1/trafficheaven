4b74d7dc54a9eb1ec3a959f880790f51ef7004b1
Luke Thomas <lukethethomas@gmail.com>
2017-10-21 04:45:43 -0700

    first commit

---
+++ DataInput.py
@@ -0,0 +1,30 @@
+import random
+from Direction import Direction
+
+class DataInput:
+    def __init__(self, this_direction):
+        self.direction = this_direction
+        self.max_wait = 0
+        self.avg_wait = 0
+        self.num_cars_waiting = 0
+
+    def randomize(self):
+        self.num_cars_waiting = random.randint(0, 10)
+        if self.num_cars_waiting == 0:
+            self.avg_wait = 0
+            self.max_wait = 0
+        else:
+            self.avg_wait = random.randint(1, 60)
+            self.max_wait = random.randint(self.avg_wait, self.avg_wait + 90)
+
+    def get_cost(self):
+        max_wait_cost = (self.max_wait - 100) * 3
+        max_wait_cost = max_wait_cost if max_wait_cost > 0 else 0
+        return self.avg_wait * self.num_cars_waiting + max_wait_cost
+
+    def get_inputs(self):
+        return [self.max_wait, self.avg_wait, self.num_cars_waiting]
+
+    def to_string(self):
+        tuple_rep = (self.max_wait, self.avg_wait, self.num_cars_waiting)
+        return str(tuple_rep)
---
+++ StopLight.py
@@ -0,0 +1,50 @@
+import operator
+from DataInput import DataInput
+from Direction import Direction
+from LightState import LightState
+
+class StopLight:
+    def __init__(self):
+        self.direction_inputs = {}
+        self.costs = {}
+        self.state_costs = {}
+
+    def fill(self):
+        for direction in Direction:
+            self.direction_inputs[direction] = DataInput(direction)
+
+    def randomize(self):
+        for key in self.direction_inputs.keys():
+            self.direction_inputs[key].randomize()
+
+    def calc_costs(self):
+        for key in self.direction_inputs.keys():
+            self.costs[key] = self.direction_inputs[key].get_cost()
+
+    def determine_correct_state(self):
+        for state in LightState:
+            self.state_costs[state] = 0
+            for key in state.value:
+                self.state_costs[state] += self.costs[key]
+        sorted_costs = sorted(self.state_costs.items(), key=operator.itemgetter(1))
+        return sorted_costs[-1][0]
+
+    def get_input(self):
+        input_list = []
+        for key in self.costs:
+            input_list += self.direction_inputs[key].get_inputs()
+        return input_list
+    
+    def get_output(self):
+        return self.determine_correct_state()
+
+    def print_costs(self):
+        for key in self.costs:
+            print(key.name + ": ")
+            print("Inputs: " + str(self.direction_inputs[key].to_string()))
+            print("Cost: " + str(self.costs[key]))
+            print()
+
+    def print_state_costs(self):
+        for key in self.state_costs:
+            print(str(key) + ": " + str(self.state_costs[key]))
